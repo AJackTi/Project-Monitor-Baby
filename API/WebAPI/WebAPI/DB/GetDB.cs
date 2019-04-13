6e0397764146c9bcdb1289c18bf12c1ca8d3cbe4
AJackTi <dtrong97vn@gmail.com>
2019-04-13 20:51:31 +0700

    update Web API C#
    update Web API C#

---
+++ API/WebAPI/WebAPI/DB/GetDB.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace WebAPI.DB
+{
+    public class GetDB
+    {
+        private static InitDB initDB;
+        private static SQLiteCommand sql_cmd;
+        private static SQLiteConnection sql_con;
+        private static SQLiteDataAdapter DB;
+        private static DataSet DS = new DataSet();
+        private static DataTable DT = new DataTable();
+        public GetDB()
+        {
+            initDB = new InitDB();
+            DB = new SQLiteDataAdapter();
+            //initDB.SetConnection();
+            sql_cmd = new SQLiteCommand();
+        }
+        public static DataTable GetAll(string dbName)
+        {
+            sql_con = initDB.SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select * from " + dbName;
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            return DT;
+        }
+
+        public static string ConvertDataTableToString(DataTable dataTable)
+        {
+            string data = string.Empty;
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                DataRow row = dataTable.Rows[i];
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    data += dataTable.Columns[j].ColumnName + "~" + row[j];
+                    if (j == dataTable.Columns.Count - 1)
+                    {
+                        if (i != (dataTable.Rows.Count - 1))
+                            data += "$";
+                    }
+                    else
+                        data += "|";
+                }
+            }
+            return data;
+        }
+
+        public static bool GetWithInfo(IEnumerable<dynamic> listOutput, IEnumerable<dynamic> listInput, IEnumerable<dynamic> listParameter, string dbName)
+        {
+            sql_con = initDB.SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select ";
+            //sql_con.Close();
+            foreach (dynamic item in listOutput)
+            {
+                CommandText += item;
+                if (listOutput.ToList().IndexOf(item) != listOutput.Count())
+                {
+                    CommandText += ", ";
+                }
+            }
+            CommandText += " from " + dbName + " ";
+            for (int i = 0; i < listInput.Count(); i++)
+            {
+                CommandText += "where " + listParameter.ToList()[i].toString() + "=" + listInput.ToList()[i].toString();
+            }
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            if (ConvertDataTableToString(DT)!=string.Empty)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ API/WebAPI/WebAPI/DB/PostDB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace WebAPI.DB
+{
+    public class PostDB
+    {
+        private InitDB initDB;
+        private SQLiteCommand sql_cmd;
+        private SQLiteConnection sql_con;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+        public PostDB()
+        {
+            initDB = new InitDB();
+            DB = new SQLiteDataAdapter();
+            //initDB.SetConnection();
+            sql_cmd = new SQLiteCommand();
+        }
+        private bool Post(IEnumerable<dynamic> listInput, IEnumerable<dynamic> listParameter, string dbName)
+        {
+            sql_con = initDB.SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "INSERT INTO " + dbName + "(";
+            for (int i = 0; i < listParameter.Count(); i++)
+            {
+                if (i != listParameter.Count()-1)
+                {
+                    CommandText += listParameter.ToList()[i] + ", ";
+                }
+                else
+                {
+                    CommandText += listParameter.ToList()[i] + ") VALUES(";
+                }
+            }
+            for (int i = 0; i < listInput.Count(); i++)
+            {
+                if (i != listInput.Count() - 1)
+                {
+                    CommandText += listInput.ToList()[i] + ", ";
+                }
+                else
+                {
+                    CommandText += listParameter.ToList()[i] + ")";
+                }
+            }
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            //DB.Fill(DS);
+            //DT = DS.Tables[0];
+            //return DT;
+            return false;
+        }
+    }
+}
