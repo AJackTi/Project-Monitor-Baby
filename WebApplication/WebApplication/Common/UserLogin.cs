faa3919cb31989a61bb93c6d6e92088f95094853
Đặng Võ Đức Trọng <dtrong97vn@gmail.com>
2019-05-09 07:35:02 +0700

    update
    update

---
+++ WebApplication/WebApplication/Common/UserLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApplication.Common
+{
+    [Serializable]
+    public class UserLogin
+    {
+        public string UserName { set; get; }
+        public string Password { get; set; }
+    }
+}
